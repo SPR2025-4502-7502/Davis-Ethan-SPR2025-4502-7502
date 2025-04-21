59ba085bde2932c1b30806faf9990882b55331a8
Will Lager <iniwil@users.noreply.github.com>
2025-04-21 11:42:07 -0400

    Update gallery.js

--- js/gallery.js
+++ js/gallery.js
@@ -1,43 +1,43 @@
-	"use strict";
+"use strict";
 
-	var photoDivs = document.getElementsByClassName("gallery-image");
+var photoDivs = document.getElementsByClassName("gallery-image");
 
-	var nextButton = document.getElementById("button_next");
+var nextButton = document.getElementById("button_next");
 
-	var previousButton = document.getElementById("button_previous");
+var previousButton = document.getElementById("button_previous");
 
-	var photoNumber = document.getElementById("stepper");
+var photoNumber = document.getElementById("stepper");
 
-	var currentPhotoNumber = 0;
+var currentPhotoNumber = 0;
 
-	photoDivs[currentPhotoNumber].classList.remove('hideThis');
+photoDivs[currentPhotoNumber].classList.remove('hideThis');
 
-	photoNumber.innerHTML = (currentPhotoNumber + 1) + " / " + photoDivs.length;
+photoNumber.innerHTML = (currentPhotoNumber + 1) + " / " + photoDivs.length;
 
-	nextButton.addEventListener('click', function()[
-		photoDivs[currentPhotoNumber].classList.add("hideThis");
-		currentPhotoNumber = currentPhotoNumber + 1;
+nextButton.addEventListener('click', function() {
+  photoDivs[currentPhotoNumber].classList.add("hideThis");
+  currentPhotoNumber = currentPhotoNumber + 1;
 
-		if (currentPhotoNumber === photoDivs.length){
-			currentPhotoNumber = 0;
-		}
+  if (currentPhotoNumber === photoDivs.length) {
+    currentPhotoNumber = 0;
+  }
 
-		photoDivs[currentPhotoNumber].classList.remove('hideThis');
+  photoDivs[currentPhotoNumber].classList.remove('hideThis');
 
-		photoNumber.innerHTML = (currentPhotoNumber + 1) + " / " + photoDivs.length;
+  photoNumber.innerHTML = (currentPhotoNumber + 1) + " / " + photoDivs.length;
 
-	});
+});
 
-	previousButton.addEventListener('click', function(){
-		photoDivs[currentPhotoNumber].classList.add("hideThis");
-		currentPhotoNumber = currentPhotoNumber - 1;
+previousButton.addEventListener('click', function() {
+  photoDivs[currentPhotoNumber].classList.add("hideThis");
+  currentPhotoNumber = currentPhotoNumber - 1;
 
-		if (currentPhotoNumber < 0) {
-			currentPhotoNumber = photoDivs.length - 1;
-		}
+  if (currentPhotoNumber < 0) {
+    currentPhotoNumber = photoDivs.length - 1;
+  }
 
-		photoDivs[currentPhotoNumber].classList.remove('hideThis');
+  photoDivs[currentPhotoNumber].classList.remove('hideThis');
 
-		photoNumber.innerHTML = (currentPhotoNumber +1) + " / " + photoDivs.length;
+  photoNumber.innerHTML = (currentPhotoNumber + 1) + " / " + photoDivs.length;
 
-	});
+});
